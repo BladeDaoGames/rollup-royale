dcd2bb6b358a58c0302f1c39944d32da59d11492
0xlonescv <0xbuilderham@gmail.com>
2023-08-26 19:55:10 +0800

    :sparkles: add wallet balance amt to navbar status

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -5,9 +5,13 @@ import {SignUpButton} from '../ConnectWalletButton/SignUpButton';
 //import {VscFeedback} from 'react-icons/vsc';
 import {createDevPrivateKey} from '../../atoms';
 import { useSetAtom } from 'jotai';
+import { useAccount, useBalance } from 'wagmi';
 
 const CustomNavBar = () => {
-    const setDevPk = useSetAtom(createDevPrivateKey)
+    const setDevPk = useSetAtom(createDevPrivateKey);
+    const { address, isConnected } = useAccount()
+    const { data, isError, isLoading } = useBalance({address, cacheTime: 2_000,})
+    
     return (
         <Navbar className='top-0 left-0 z-20 w-full py-0 bg-background1'>
             <Navbar.Brand href={import.meta.env.VITE_HOSTSITE || "localhost:3000"}>
@@ -19,13 +23,23 @@ const CustomNavBar = () => {
                 <span className="text-2xl italic font-bold sm:text-3xl text-prime2">
                     Loot Royale</span>
                 </Navbar.Brand>
+                
+                {/* Dev tooling */}
                 {(import.meta.env.VITE_ENV == "dev")&&
             <button className="w-10 h-10 bg-red-700"
             onClick={()=>{setDevPk("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d")}}></button>}
+
+
             <Navbar.Collapse className="ml-auto mr-2">
                 <Navbar.Link href="#">
                     <div className="text-sm font-medium  text-prime2 md:text-background1 md:bg-lightbeige md:rounded-lg md:text-base md:px-4 md:py-2 md:text-center md:hover:bg-darkbeige">
                         Game Rules</div>
+                </Navbar.Link>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="mr-2">
+                <Navbar.Link href="#">
+                    <div className="text-sm font-medium  text-prime2 md:text-background1 md:bg-prime2 md:rounded-lg md:text-base md:px-4 md:py-2 md:text-center md:hover:bg-darkbeige">
+                        {`$BLADE: ${data?.formatted??"??"}`}</div>
                 </Navbar.Link>
             </Navbar.Collapse>
             <Navbar.Collapse className="mr-2">
