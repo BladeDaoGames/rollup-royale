c79ef9f103b8425f7ef6530d06e8f3c68a7db75e
0xlonescv <0xbuilderham@gmail.com>
2023-09-04 23:23:54 +0800

    :triangular_flag_on_post: update proper balance button to link with alt layer faucet

---
+++ client/src/components/Navbar/BalanceButton.tsx
@@ -0,0 +1,28 @@
+import React, {useMemo} from 'react';
+import { useAccount, useBalance, useNetwork } from 'wagmi';
+import { useBurnerKey } from '../../hooks/useBurnerKey';
+import { chainConfig } from '../../config/chainConfig';
+
+export const BalanceButton = () => {
+    const { address, isConnected } = useAccount()
+    const { data, isError, isLoading } = useBalance({address, watch: true})
+    const { chain, chains } = useNetwork()
+    const { burnerKey, burnerAddress, updateBurnerKey} = useBurnerKey();
+    const hasBurnerKey = burnerKey !==null
+    const usingBurner = hasBurnerKey && (
+        address?.toLowerCase()==burnerAddress?.toLowerCase()) && isConnected
+
+    return useMemo(()=>(
+        <div className={`text-sm font-medium  text-prime2 md:text-background1 
+            ${(usingBurner && parseInt(data?.formatted)==0)?"md:bg-palered text-white":"md:bg-prime2"}
+            md:rounded-lg md:text-base md:px-4 md:py-2 md:text-center md:hover:bg-darkbeige
+            `}>
+                {
+                (!isConnected || chain?.id != chainConfig.chaindetails?.id)?"Wrong Network":
+                (usingBurner && parseInt(data?.formatted)==0)?"Click Here 2 Get Gas From Faucet Link":
+                `${chain?.nativeCurrency.symbol??"Balance"}: ${
+                    data?.formatted?parseFloat(data?.formatted).toFixed(3):"0"
+                    }`
+                }</div>
+    ),[address, isConnected, usingBurner, data?.formatted, chain?.nativeCurrency.symbol, chain?.id])
+}
