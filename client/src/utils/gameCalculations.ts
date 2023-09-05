f0159fe247738b48090f2265da0243f42dc14d60
Peter Liang <peter.liang.official@gmail.com>
2023-09-05 22:12:54 +1000

    initial commit - removed client

--- client/src/utils/gameCalculations.ts
+++
@@ -1,9 +0,0 @@
-export const boardPositionToGameTileXY = (boardPos:number)=>{
-    const boardWidth=10
-    const boardHeight=10
-
-    const TileY = Math.floor(boardPos / boardHeight)
-    const TileX = boardPos % boardHeight
-
-    return {x:TileX, y:TileY}
-}
