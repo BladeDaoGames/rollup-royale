e56bbeff714221fa7b0afcb7008f1a4918cde3f2
0xlonescv <0xbuilderham@gmail.com>
2023-09-11 17:25:34 +0800

    :pencil2: sc: fix typo on import

--- contracts/src/RRoyale.sol
+++ contracts/src/RRoyale.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 //import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
