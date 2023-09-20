447e22c7f890d55014ce7c7d24a961192baaeab5
0xlonescv <0xbuilderham@gmail.com>
2023-09-20 17:15:36 +0800

    :wrench: add a contract error map for error handling

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,1 +1,22 @@
-export const ROYALE_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const ROYALE_ADDRESS = "0xbCc7BFBF85FBA23c1008c23aA35d3303e34372A6";
+
+export const ContractErrorMap = {
+    "E1": "World is paused",
+    "E2": "Insufficient funds for transaction",
+    "E3": "Player is already in a game",
+    "E4": "Game player count full",
+    "E5": "Game has already started",
+    "E6": "Game has already ended",
+    "E7": "Game has been abandoned",
+    "E8": "Game has not started",
+    "E9": "Not Enough Players in Game",
+    "E10": "Not all players are ready",
+    "E11": "Player not in this game",
+    "E12": "Cannot find player's id in game",
+    "E13": "Player is dead",
+    "E14": "Move is Out Of Bounds",
+    "E15": "Move is Obstructed",
+    "E16": "Prize not sent, Funds Not SAFU.",
+    "E17": "Msg sender not Allowed Burner Wallet",
+    "E18": "Game is paused",
+}
