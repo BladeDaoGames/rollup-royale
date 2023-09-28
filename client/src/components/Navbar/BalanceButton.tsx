48b1edfa5776f38658e59e1bddd7fb7d1322f349
0xlonescv <0xbuilderham@gmail.com>
2023-09-28 11:54:04 +0800

    :ambulance: update balance notic to tell them they don't have gas

--- client/src/components/Navbar/BalanceButton.tsx
+++ client/src/components/Navbar/BalanceButton.tsx
@@ -13,13 +13,14 @@ export const BalanceButton = () => {
         address?.toLowerCase()==burnerAddress?.toLowerCase()) && isConnected
 
     return useMemo(()=>(
-        <div className={`text-sm font-medium  text-prime2 md:text-background1 
-            ${(usingBurner && parseInt(data?.formatted)==0)?"md:bg-palered text-white":"md:bg-prime2"}
+        <div className={`text-sm font-medium   
+            ${(usingBurner && parseInt(data?.formatted)==0)?"md:bg-palered text-white":
+            "md:bg-prime2 text-prime2 md:text-background1"}
             md:rounded-lg md:text-base md:px-4 md:py-2 md:text-center md:hover:bg-darkbeige
             `}>
                 {
                 (!isConnected || chain?.id != chainConfig.chaindetails?.id)?"Wrong Network":
-                (usingBurner && parseInt(data?.formatted)==0)?"Click Here 2 Get Gas From Faucet Link":
+                (usingBurner && parseInt(data?.formatted)==0)?"U have 0 $BLADE in burner. Click here for faucet!":
                 `${chain?.nativeCurrency.symbol??"Balance"}: ${
                     data?.formatted?parseFloat(data?.formatted).toFixed(3):"0"
                     }`
